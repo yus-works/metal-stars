6e321f11b56f35a68ddb60446f819094a8b0acf1
tedord25 <djuric.teodor25@gmail.com>
2024-07-20 09:13:07 +0200

    impl lifecycle methods as toplevel funcs

--- app/src/main/java/com/example/metalstars/common/SessionLifecycleHelper.kt
+++ app/src/main/java/com/example/metalstars/common/SessionLifecycleHelper.kt
@@ -8,6 +8,11 @@ import androidx.core.content.ContextCompat
 import android.content.Intent
 import android.provider.Settings
 import android.net.Uri
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.Session
+import com.google.ar.core.exceptions.CameraNotAvailableException
 
 
 var CAMERA_PERMISSION_CODE = 0;
@@ -40,4 +45,70 @@ fun launchPermissionSettings(activity: Activity) {
     intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
     intent.setData(Uri.fromParts("package", activity.packageName, null));
     activity.startActivity(intent);
+}
+
+var installRequested = false
+
+var exceptionCallback: ((Exception) -> Unit)? = null
+var beforeSessionResume: ((Session) -> Unit)? = null
+
+fun tryCreateSession(activity: Activity, features: Set<Session.Feature> = setOf()): Session? {
+    if (!hasCameraPermission(activity)) {
+        requestCameraPermission(activity)
+        return null
+    }
+
+    return try {
+        // Request installation if necessary.
+        when (ArCoreApk.getInstance().requestInstall(activity, !installRequested)!!) {
+            ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
+                installRequested = true
+                // tryCreateSession will be called again, so we return null for now.
+                return null
+            }
+            ArCoreApk.InstallStatus.INSTALLED -> {
+                // Left empty; nothing needs to be done.
+            }
+        }
+
+        // Create a session if Google Play Services for AR is installed and up to date.
+        Session(activity, features)
+    } catch (e: Exception) {
+        exceptionCallback?.invoke(e)
+        null
+    }
+}
+
+fun onResume(activity: Activity, session: Session?): Session? {
+    val newSession = session ?: tryCreateSession(activity) ?: return null
+    return try {
+        beforeSessionResume?.invoke(newSession)
+        newSession.resume()
+        newSession
+    } catch (e: CameraNotAvailableException) {
+        exceptionCallback?.invoke(e)
+        null
+    }
+}
+
+fun onPause(session: Session?) {
+    session?.pause()
+}
+
+fun onDestroy(session: Session?) {
+    session?.close()
+}
+
+fun onRequestPermissionsResult(activity: Activity) {
+    if (!hasCameraPermission(activity)) {
+        Toast.makeText(
+            activity,
+            "Camera permission is needed to run this application",
+            Toast.LENGTH_LONG
+        ).show()
+        if (!shouldShowRequestPermissionRationale(activity)) {
+            launchPermissionSettings(activity)
+        }
+        activity.finish()
+    }
 }
