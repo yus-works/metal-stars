689e9879d67b1b0fce401af5bcb2189b3f37f2cb
tedord25 <djuric.teodor25@gmail.com>
2024-07-22 07:23:21 +0200

    refactor box into separate composable

--- app/src/main/java/com/example/metalstars/MainActivity.kt
+++ app/src/main/java/com/example/metalstars/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -32,6 +33,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.metalstars.ui.theme.MetalStarsTheme
 import com.google.android.filament.Engine
+import com.google.android.filament.View
 import com.google.ar.core.Anchor
 import com.google.ar.core.Config
 import com.google.ar.core.Frame
@@ -42,13 +44,16 @@ import io.github.sceneview.ar.arcore.createAnchorOrNull
 import io.github.sceneview.ar.arcore.getUpdatedPlanes
 import io.github.sceneview.ar.arcore.isValid
 import io.github.sceneview.ar.getDescription
+import io.github.sceneview.ar.node.ARCameraNode
 import io.github.sceneview.ar.node.AnchorNode
 import io.github.sceneview.ar.rememberARCameraNode
+import io.github.sceneview.collision.CollisionSystem
 import io.github.sceneview.loaders.MaterialLoader
 import io.github.sceneview.loaders.ModelLoader
 import io.github.sceneview.model.ModelInstance
 import io.github.sceneview.node.CubeNode
 import io.github.sceneview.node.ModelNode
+import io.github.sceneview.node.Node
 import io.github.sceneview.rememberCollisionSystem
 import io.github.sceneview.rememberEngine
 import io.github.sceneview.rememberMaterialLoader
@@ -63,112 +68,117 @@ private const val kMaxModelInstances = 10
 
 class MainActivity : ComponentActivity() {
 
+    @Composable
+    private fun SurfaceContainer() {
+        // A surface container using the 'background' color from the theme
+        Box(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            // The destroy calls are automatically made when their disposable effect leaves
+            // the composition or its key changes.
+            val engine = rememberEngine()
+            val modelLoader = rememberModelLoader(engine)
+            val materialLoader = rememberMaterialLoader(engine)
+            val cameraNode = rememberARCameraNode(engine)
+            val childNodes = rememberNodes()
+            val view = rememberView(engine)
+            val collisionSystem = rememberCollisionSystem(view)
+
+            var planeRenderer by remember { mutableStateOf(true) }
+
+            val modelInstances = remember { mutableListOf<ModelInstance>() }
+            var trackingFailureReason by remember {
+                mutableStateOf<TrackingFailureReason?>(null)
+            }
+            var frame by remember { mutableStateOf<Frame?>(null) }
+            ARScene(
+                modifier = Modifier.fillMaxSize(),
+                childNodes = childNodes,
+                engine = engine,
+                view = view,
+                modelLoader = modelLoader,
+                collisionSystem = collisionSystem,
+                sessionConfiguration = { session, config ->
+                    config.depthMode =
+                        when (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+                            true -> Config.DepthMode.AUTOMATIC
+                            else -> Config.DepthMode.DISABLED
+                        }
+                    config.instantPlacementMode = Config.InstantPlacementMode.LOCAL_Y_UP
+                    config.lightEstimationMode =
+                        Config.LightEstimationMode.ENVIRONMENTAL_HDR
+                },
+                cameraNode = cameraNode,
+                planeRenderer = planeRenderer,
+                onTrackingFailureChanged = {
+                    trackingFailureReason = it
+                },
+                onSessionUpdated = { session, updatedFrame ->
+                    frame = updatedFrame
+
+                    if (childNodes.isEmpty()) {
+                        updatedFrame.getUpdatedPlanes()
+                            .firstOrNull { it.type == Plane.Type.HORIZONTAL_UPWARD_FACING }
+                            ?.let { it.createAnchorOrNull(it.centerPose) }?.let { anchor ->
+                                childNodes += createAnchorNode(
+                                    engine = engine,
+                                    modelLoader = modelLoader,
+                                    materialLoader = materialLoader,
+                                    modelInstances = modelInstances,
+                                    anchor = anchor
+                                )
+                            }
+                    }
+                },
+                onGestureListener = rememberOnGestureListener(
+                    onSingleTapConfirmed = { motionEvent, node ->
+                        if (node == null) {
+                            val hitResults = frame?.hitTest(motionEvent.x, motionEvent.y)
+                            hitResults?.firstOrNull {
+                                it.isValid(
+                                    depthPoint = false,
+                                    point = false
+                                )
+                            }?.createAnchorOrNull()
+                                ?.let { anchor ->
+                                    planeRenderer = false
+                                    childNodes += createAnchorNode(
+                                        engine = engine,
+                                        modelLoader = modelLoader,
+                                        materialLoader = materialLoader,
+                                        modelInstances = modelInstances,
+                                        anchor = anchor
+                                    )
+                                }
+                        }
+                    })
+            )
+            Text(
+                modifier = Modifier
+                    .systemBarsPadding()
+                    .fillMaxWidth()
+                    .align(Alignment.TopCenter)
+                    .padding(top = 16.dp, start = 32.dp, end = 32.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 28.sp,
+                color = Color.White,
+                text = trackingFailureReason?.let {
+                    it.getDescription(LocalContext.current)
+                } ?: if (childNodes.isEmpty()) {
+                    "Point your camera at a flat surface"
+                } else {
+                    "Tap on a surface to place the model"
+                }
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             MetalStarsTheme {
-                // A surface container using the 'background' color from the theme
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-                    // The destroy calls are automatically made when their disposable effect leaves
-                    // the composition or its key changes.
-                    val engine = rememberEngine()
-                    val modelLoader = rememberModelLoader(engine)
-                    val materialLoader = rememberMaterialLoader(engine)
-                    val cameraNode = rememberARCameraNode(engine)
-                    val childNodes = rememberNodes()
-                    val view = rememberView(engine)
-                    val collisionSystem = rememberCollisionSystem(view)
-
-                    var planeRenderer by remember { mutableStateOf(true) }
-
-                    val modelInstances = remember { mutableListOf<ModelInstance>() }
-                    var trackingFailureReason by remember {
-                        mutableStateOf<TrackingFailureReason?>(null)
-                    }
-                    var frame by remember { mutableStateOf<Frame?>(null) }
-                    ARScene(
-                        modifier = Modifier.fillMaxSize(),
-                        childNodes = childNodes,
-                        engine = engine,
-                        view = view,
-                        modelLoader = modelLoader,
-                        collisionSystem = collisionSystem,
-                        sessionConfiguration = { session, config ->
-                            config.depthMode =
-                                when (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
-                                    true -> Config.DepthMode.AUTOMATIC
-                                    else -> Config.DepthMode.DISABLED
-                                }
-                            config.instantPlacementMode = Config.InstantPlacementMode.LOCAL_Y_UP
-                            config.lightEstimationMode =
-                                Config.LightEstimationMode.ENVIRONMENTAL_HDR
-                        },
-                        cameraNode = cameraNode,
-                        planeRenderer = planeRenderer,
-                        onTrackingFailureChanged = {
-                            trackingFailureReason = it
-                        },
-                        onSessionUpdated = { session, updatedFrame ->
-                            frame = updatedFrame
-
-                            if (childNodes.isEmpty()) {
-                                updatedFrame.getUpdatedPlanes()
-                                    .firstOrNull { it.type == Plane.Type.HORIZONTAL_UPWARD_FACING }
-                                    ?.let { it.createAnchorOrNull(it.centerPose) }?.let { anchor ->
-                                        childNodes += createAnchorNode(
-                                            engine = engine,
-                                            modelLoader = modelLoader,
-                                            materialLoader = materialLoader,
-                                            modelInstances = modelInstances,
-                                            anchor = anchor
-                                        )
-                                    }
-                            }
-                        },
-                        onGestureListener = rememberOnGestureListener(
-                            onSingleTapConfirmed = { motionEvent, node ->
-                                if (node == null) {
-                                    val hitResults = frame?.hitTest(motionEvent.x, motionEvent.y)
-                                    hitResults?.firstOrNull {
-                                        it.isValid(
-                                            depthPoint = false,
-                                            point = false
-                                        )
-                                    }?.createAnchorOrNull()
-                                        ?.let { anchor ->
-                                            planeRenderer = false
-                                            childNodes += createAnchorNode(
-                                                engine = engine,
-                                                modelLoader = modelLoader,
-                                                materialLoader = materialLoader,
-                                                modelInstances = modelInstances,
-                                                anchor = anchor
-                                            )
-                                        }
-                                }
-                            })
-                    )
-                    Text(
-                        modifier = Modifier
-                            .systemBarsPadding()
-                            .fillMaxWidth()
-                            .align(Alignment.TopCenter)
-                            .padding(top = 16.dp, start = 32.dp, end = 32.dp),
-                        textAlign = TextAlign.Center,
-                        fontSize = 28.sp,
-                        color = Color.White,
-                        text = trackingFailureReason?.let {
-                            it.getDescription(LocalContext.current)
-                        } ?: if (childNodes.isEmpty()) {
-                            "Point your camera at a flat surface"
-                        } else {
-                            "Tap on a surface to place the model"
-                        }
-                    )
-                }
+                SurfaceContainer()
             }
         }
     }
