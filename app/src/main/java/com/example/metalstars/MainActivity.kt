61057f415e7fed537258923e672bbc39fef3a558
tedord25 <djuric.teodor25@gmail.com>
2024-07-25 12:48:49 +0200

    add stuff for checking coords and alt

--- app/src/main/java/com/example/metalstars/MainActivity.kt
+++ app/src/main/java/com/example/metalstars/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.metalstars.ui.theme.MetalStarsTheme
 import com.google.android.filament.Engine
 import com.google.android.filament.View
@@ -67,10 +68,18 @@ import io.github.sceneview.rememberNodes
 import io.github.sceneview.rememberOnGestureListener
 import io.github.sceneview.rememberView
 import java.util.Arrays
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
 
 
 private const val kModelFile = "models/simple_satellite_low_poly_free.glb"
 private const val kMaxModelInstances = 10
+
+private const val LOCATION_PERMISSION_REQUEST_CODE = 100
+
 
 class MainActivity : ComponentActivity() {
 
@@ -83,6 +92,23 @@ class MainActivity : ComponentActivity() {
         config.instantPlacementMode = Config.InstantPlacementMode.LOCAL_Y_UP
         config.lightEstimationMode =
             Config.LightEstimationMode.ENVIRONMENTAL_HDR
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            getLastKnownLocation()
+        }
+    }
+
+    private fun getLastKnownLocation(): Array<Double> {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        checkLocationPermission()
+        val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if (location != null) {
+            return arrayOf(location.latitude, location.longitude, location.altitude)
+        }
     }
 
     @Composable
@@ -212,6 +238,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        checkLocationPermission()
         setContent {
             MetalStarsTheme {
                 SurfaceContainer()
